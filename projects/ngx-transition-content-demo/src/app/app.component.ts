b5618ce4d2d3b56ac0a58d8eb05c692748127426
Philipp Bauer <ciriousjoker@gmail.com>
2026-07-06 23:24:11 +0200

    refactor: modernize demo application

--- projects/ngx-transition-content-demo/src/app/app.component.ts
+++ projects/ngx-transition-content-demo/src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, signal } from "@angular/core";
+import { NgxTransitionContentComponent, NgxTransitionContentPage } from "ngx-transition-content";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  imports: [NgxTransitionContentComponent, NgxTransitionContentPage],
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.scss",
+  standalone: true,
 })
 export class AppComponent {
-  public slot = 0;
+  public readonly slot = signal(0);
+
+  public toggleSlot() {
+    this.slot.update((slot) => (slot === 1 ? 0 : 1));
+  }
 }
